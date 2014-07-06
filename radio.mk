0282aa6cd04b45b6c588912e2ba1b0233945454d
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-07-06 19:28:12 +0200

    shinano: update radio prop
    Change-Id: I7c8c628b6db693d2a7aa3614e17d597c59cc9347

--- radio.mk
+++ radio.mk
@@ -2,23 +2,15 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=9 \
     telephony.lteOnCdmaDevice=0 \
-    telephony.lteOnGsmDevice=1 \
     persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.add_power_save=1 \
-    persist.radio.mode_pref_nv10=1
+    persist.radio.dont_use_dsd=false \
+    persist.radio.eons.enabled=false \
+    persist.radio.oem_socket=false
 
 # Ril sends only one RIL_UNSOL_CALL_RING, so set call_ring.multiple to false
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.call_ring.multiple=0
 
-# System props for the data modules
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.netmgrd.qos.enable=false
-
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so
-
-# update 1x signal strength after 2s
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.radio.snapshot_enabled=1 \
-    persist.radio.snapshot_timer=2
