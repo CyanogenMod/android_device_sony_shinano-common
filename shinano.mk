99d5971da48055d9b5cd92e1a2fcd8d71f87852b
Shane Francis <bigbeeshane@gmail.com>
2014-12-13 00:19:43 +0000

    audio: Updates for open source HAL
    * Add audio_platform_info.xml for speaker backend
    
    Change-Id: I473410a2e77f187ceeafb098737dbb7f99214ec8

--- shinano.mk
+++ shinano.mk
@@ -65,6 +65,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libaudioamp
 
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
+
 # Bluetooth
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
