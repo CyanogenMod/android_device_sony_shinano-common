26c28c580f99de47e0537e41cad5583b8d4fcbe2
Chirayu Desai <chirayudesai1@gmail.com>
2015-02-02 19:52:25 +0530

    Misc Cleanup
    * We have a broadcom chip.
    
    Change-Id: I7223373b11abe15d98e8e8bb36a07ff56374fa2c

--- shinano.mk
+++ shinano.mk
@@ -77,10 +77,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     e2fsck
 
-# FM Radio
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/system/etc/init.qcom.fm.sh:system/etc/init.qcom.fm.sh
-
 # GPS
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/gps/flp.conf:system/etc/flp.conf \
