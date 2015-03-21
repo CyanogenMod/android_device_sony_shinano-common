a58337fd646de90ccfbf99e29e4e52eacdae2297
Chirayu Desai <chirayudesai1@gmail.com>
2015-03-21 12:17:10 +0530

    audio: Switch back to proprietary HAL
    * The open source HAL works perfect for media,
      but in-call audio is wonky.
    * Because let's rather have the phone part of our smartphones
      work perfectly.
    
    Change-Id: I581f2a6210115d0bdf64c44373a1bf29effeb9e8

--- shinano.mk
+++ shinano.mk
@@ -58,15 +58,6 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
-# Audio
-PRODUCT_PACKAGES += \
-     tfa9890_amp
-#    libaudioamp
-
-# Audio configuration
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
-
 # Bluetooth
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
