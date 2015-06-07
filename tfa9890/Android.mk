4f68c6c8a4b73652b1b945de4b0a96820857d0b3
Ethan Chen <intervigil@gmail.com>
2015-06-07 12:02:33 -0700

    shinano: Clean up TFA9890 helper library
    Change-Id: I6c238042f2d15e621aa25fc8edac6f8169ea0e1c

--- tfa9890/Android.mk
+++ tfa9890/Android.mk
@@ -3,24 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SHARED_LIBRARIES := \
-	liblog libutils libcutils
-
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-
-LOCAL_SRC_FILES := \
-	tfa9890.c \
-	audio_amplifier.c
-
-LOCAL_MODULE := libaudioamp
-
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SHARED_LIBRARIES := \
         liblog libutils libcutils
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
