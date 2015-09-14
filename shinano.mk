6760038d4717988c1d5ded4cac4cab1480b6c31c
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2015-09-14 13:39:24 +0200

    msm8974: Use the new power modes configuration
    Change-Id: I20039b485b2b92c40cdca5d79524a58671b1cea3

--- shinano.mk
+++ shinano.mk
@@ -41,6 +41,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
     $(COMMON_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
+    $(COMMON_PATH)/rootdir/init.qcom.power.rc:root/init.qcom.power.rc \
     $(COMMON_PATH)/rootdir/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
     $(COMMON_PATH)/rootdir/init.sony.usb.rc:root/init.sony.usb.rc \
     $(COMMON_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
@@ -173,6 +174,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # HDMI
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.demo.hdmirotationlock=false
+
+# IO Scheduler
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.io.scheduler=row
 
 # Time
 PRODUCT_PROPERTY_OVERRIDES += \
