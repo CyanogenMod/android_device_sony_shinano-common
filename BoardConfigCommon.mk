df006fcd276a0ab7395f6bfd0ada6e66fbb0115f
Chirayu Desai <chirayudesai1@gmail.com>
2016-01-23 12:27:29 +0530

    Remove stock camera stuff
    * These blobs are old (4.3), and didn't work with 6.0
    * Any of newer stock blobs don't work due to DRM (we had it working
      once, but never after that)
    * We've switched to the 3.10 kernel for camera anyways, so stock blobs
      wouldn't work anymore.
    * AOSP blobs to be added when available from Sony.
    
    Change-Id: Ie15c02cc23d8ec5afa238087d4b15ab1a529d247

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,11 +42,6 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/shinano-common/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/sony/shinano-common/bluetooth/vnd_shinano.txt
 
-# Camera
-TARGET_RELEASE_CPPFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
-BOARD_CAMERA_HAVE_ISO := true
-USE_DEVICE_SPECIFIC_CAMERA := true
-
 # CM Hardware
 BOARD_HARDWARE_CLASS += device/sony/shinano-common/cmhw
 
